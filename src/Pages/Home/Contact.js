69b701f0b17c6958823233cd1295c1cd1f1b421f
Raiyan109 <kraiyan109@gmail.com>
2023-02-05 17:28:22 +0600

    contact me page and funcionility added

---
+++ src/Pages/Home/Contact.js
@@ -0,0 +1,42 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+const Contact = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_t3rlyd6', 'template_k8rrwcu', form.current, 'W_0i0So-npwUb6b5D')
+            .then((result) => {
+                console.log(result.text);
+                e.target.reset()
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
+    return (
+        <div className='m-4'>
+            <form ref={form} onSubmit={sendEmail}>
+                <div className="mb-6">
+                    <label for="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Name</label>
+                    <input type="text" name='user_name' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required />
+                </div>
+                <div className="mb-6">
+                    <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Email</label>
+                    <input type="email" id="email" name="user_email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required />
+                </div>
+                <div className="mb-6">
+                    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+                    <textarea name="message" id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+                </div>
+                <button type="submit" value="Send" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send</button>
+            </form>
+
+        </div>
+    );
+};
+
+export default Contact;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ManageInventoriesButton from '../../ManageInventoriesButton/ManageInventoriesButton';
 import Banner from '../Banner/Banner';
 import CallToAction from '../CallToAction/CallToAction';
+import Contact from '../Contact';
 import DesignOwn from '../DesignOwn/DesignOwn';
 import Items from '../Items/Items';
 import homeStyle from './Home.module.css'
@@ -13,6 +14,7 @@ const Home = () => {
             <ManageInventoriesButton></ManageInventoriesButton>
             <CallToAction></CallToAction>
             <DesignOwn></DesignOwn>
+            <Contact />
         </div>
     );
 };
